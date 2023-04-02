95d3370694579177cd1380e3efd74909f2dc693e
Dieggo19 <diego.acuna.j@usach.cl>
2023-04-02 15:47:45 -0400

    respuesta 1 resuelta, más los scripts para responder la consulta

--- runStatements.sql
+++ runStatements.sql
@@ -1,6 +1,30 @@
 -- Inicio
 -- Preguntas 1 y 2
 
+-- Respuesta 1:
+
+WITH ventas_2021 AS (
+  SELECT id_venta, fecha, EXTRACT(MONTH FROM fecha) AS mes
+  FROM venta
+  WHERE EXTRACT(YEAR FROM fecha) = 2021
+), ventas_producto AS (
+  SELECT v.mes, pv.id_producto, COUNT(*) AS total_ventas
+  FROM ventas_2021 v
+  JOIN prod_venta pv ON pv.id_venta = v.id_venta
+  GROUP BY v.mes, pv.id_producto
+), productos_mes AS (
+  SELECT vp.mes, p.nombre, vp.total_ventas,
+         ROW_NUMBER() OVER (PARTITION BY vp.mes ORDER BY vp.total_ventas DESC) AS ranking
+  FROM ventas_producto vp
+  JOIN producto p ON p.id_producto = vp.id_producto
+  WHERE vp.mes IS NOT NULL
+)
+SELECT pm.mes, pm.nombre AS producto_mas_vendido, pm.total_ventas
+FROM productos_mes pm
+WHERE pm.ranking = 1
+ORDER BY pm.mes;
+
+
 -- Respuesta 2:
 SELECT tienda.nombre AS nombre_tienda, producto.nombre AS nombre_producto, producto.precio
 FROM tienda
